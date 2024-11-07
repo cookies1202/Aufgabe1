1a3e239c445c4d433b117f0dd269059edec8a462
haas.manuel <manuel.haas@edu.fh-joanneum.at>
2024-11-07 14:45:04 +0100

    FÜR AHMED

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,6 @@
 package org.example;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,5 +11,13 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         System.out.println("Das ist der erste test");
-        }
+
+        ArrayList<Patient> patientenListe = new ArrayList<>();
+        patientenListe.add(new Patient("001", 123456, "Anna", "Müller", LocalDate.of(1990, 5, 21)));
+        patientenListe.add(new Patient("002", 234567, "Peter", "Schmidt", LocalDate.of(1985, 8, 12)));
+        patientenListe.add(new Patient("003", 345678, "Laura", "Becker", LocalDate.of(1978, 12, 1)));
+
+
+
+    }
     }
--- src/main/java/org/example/Patient.java
+++ src/main/java/org/example/Patient.java
@@ -1,4 +1,72 @@
 package org.example;
+import java.time.LocalDate;
 
 public class Patient {
+
+    private String  fallnummer;
+    private int svNumber;
+  private     String Vorname;
+    private String Nachname;
+  private  LocalDate gebDat;
+
+    public Patient(String fallnummer, int svNumber, String vorname, String nachname, LocalDate gebDat) {
+        this.fallnummer = fallnummer;
+        this.svNumber = svNumber;
+        Vorname = vorname;
+        Nachname = nachname;
+        this.gebDat = gebDat;
+    }
+
+    public String getFallnummer() {
+        return fallnummer;
+    }
+
+    public void setFallnummer(String fallnummer) {
+        this.fallnummer = fallnummer;
+    }
+
+    public int getSvNumber() {
+        return svNumber;
+    }
+
+    public void setSvNumber(int svNumber) {
+        this.svNumber = svNumber;
+    }
+
+    public String getVorname() {
+        return Vorname;
+    }
+
+    public void setVorname(String vorname) {
+        Vorname = vorname;
+    }
+
+    public String getNachname() {
+        return Nachname;
+    }
+
+    public void setNachname(String nachname) {
+        Nachname = nachname;
+    }
+
+    public LocalDate getGebDat() {
+        return gebDat;
+    }
+
+    public void setGebDat(LocalDate gebDat) {
+        this.gebDat = gebDat;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "fallnummer='" + fallnummer + '\'' +
+                ", svNumber=" + svNumber +
+                ", Vorname='" + Vorname + '\'' +
+                ", Nachname='" + Nachname + '\'' +
+                ", gebDat=" + gebDat +
+                '}';
+    }
+
 }
+
